6bc3205394fea6f0cd7bf72423758d32c88a4327
jeksterslab <jeksterslab@users.noreply.github.com>
2024-07-08 06:50:15 +0000

    Update src to output generated at e3cce57

---
+++ tests/testthat/test-metaVAR-fit-ct-var-id-mx-theta-null.R
@@ -0,0 +1,109 @@
+## ---- test-metaVAR-fit-ct-var-id-mx-theta-null
+lapply(
+  X = 1,
+  FUN = function(i,
+                 text,
+                 tol) {
+    message(text)
+    set.seed(42)
+    n <- 2
+    time <- 500
+    delta_t <- 0.10
+    k <- p <- 3
+    iden <- diag(k)
+    null_vec <- rep(x = 0, times = k)
+    null_mat <- matrix(
+      data = 0,
+      nrow = p,
+      ncol = p
+    )
+    mu0 <- list(
+      null_vec
+    )
+    sigma0 <- diag(p)
+    sigma0_l <- list(
+      t(chol(sigma0))
+    )
+    mu <- list(
+      null_vec
+    )
+    sigma <- 0.1 * iden
+    sigma_l <- list(
+      t(chol(sigma))
+    )
+    nu <- list(
+      null_vec
+    )
+    lambda <- list(
+      iden
+    )
+    theta <- null_mat
+    theta_l <- list(
+      null_mat
+    )
+    phi_mu <- matrix(
+      data = c(
+        -0.357,
+        0.771,
+        -0.450,
+        0.0,
+        -0.511,
+        0.729,
+        0,
+        0,
+        -0.693
+      ),
+      nrow = k
+    )
+    phi_sigma <- 0.00001 * diag(p * p)
+    phi <- simStateSpace::SimPhiN(
+      n = n,
+      phi = phi_mu,
+      vcov_phi_vec_l = t(chol(phi_sigma))
+    )
+    sim <- simStateSpace::SimSSMOUIVary(
+      n = n,
+      time = time,
+      delta_t = delta_t,
+      mu0 = mu0,
+      sigma0_l = sigma0_l,
+      mu = mu,
+      phi = phi,
+      sigma_l = sigma_l,
+      nu = nu,
+      lambda = lambda,
+      theta_l = theta_l
+    )
+    data <- as.data.frame(sim)
+    fit <- fitCTVARMx::FitCTVARIDMx(
+      data = data,
+      observed = paste0("y", seq_len(k)),
+      id = "id",
+      time = "time",
+      sigma_diag = TRUE,
+      ncores = NULL
+    )
+    meta <- MetaVARMx(fit, noise = TRUE)
+    print(meta)
+    summary(meta)
+    coef(meta)
+    vcov(meta)
+    testthat::test_that(
+      text,
+      {
+        testthat::expect_true(
+          all(
+            abs(
+              c(
+                phi_mu,
+                diag(sigma)
+              ) - coef(meta)
+            ) <= tol
+          )
+        )
+      }
+    )
+  },
+  text = "test-metaVAR-fit-ct-var-id-mx-theta-null",
+  tol = 0.3
+)
---
+++ tests/testthat/test-metaVAR-fit-dt-var-id-mx-theta-null.R
@@ -0,0 +1,82 @@
+## ---- test-metaVAR-fit-dt-var-id-mx-theta-null
+lapply(
+  X = 1,
+  FUN = function(i,
+                 text,
+                 tol) {
+    message(text)
+    set.seed(42)
+    n <- 2
+    time <- 500
+    k <- p <- 3
+    iden <- diag(k)
+    null_vec <- rep(x = 0, times = k)
+    mu0 <- list(
+      null_vec
+    )
+    sigma0 <- diag(p)
+    sigma0_l <- list(
+      t(chol(sigma0))
+    )
+    alpha <- list(
+      null_vec
+    )
+    psi <- 0.1 * iden
+    psi_l <- list(
+      t(chol(psi))
+    )
+    beta_mu <- matrix(
+      data = c(
+        0.7, 0.5, -0.1,
+        0.0, 0.6, 0.4,
+        0, 0, 0.5
+      ),
+      nrow = p
+    )
+    beta_sigma <- 0.00001 * diag(p * p)
+    beta <- simStateSpace::SimBetaN(
+      n = n,
+      beta = beta_mu,
+      vcov_beta_vec_l = t(chol(beta_sigma))
+    )
+    sim <- simStateSpace::SimSSMVARIVary(
+      n = n,
+      time = time,
+      mu0 = mu0,
+      sigma0_l = sigma0_l,
+      alpha = alpha,
+      beta = beta,
+      psi_l = psi_l
+    )
+    data <- as.data.frame(sim)
+    fit <- fitDTVARMx::FitDTVARIDMx(
+      data = data,
+      observed = paste0("y", seq_len(k)),
+      id = "id",
+      psi_diag = TRUE,
+      ncores = NULL
+    )
+    meta <- MetaVARMx(fit, noise = TRUE)
+    print(meta)
+    summary(meta)
+    coef(meta)
+    vcov(meta)
+    testthat::test_that(
+      text,
+      {
+        testthat::expect_true(
+          all(
+            abs(
+              c(
+                beta_mu,
+                diag(psi)
+              ) - coef(meta)
+            ) <= tol
+          )
+        )
+      }
+    )
+  },
+  text = "test-metaVAR-fit-dt-var-id-mx-theta-null",
+  tol = 0.3
+)
